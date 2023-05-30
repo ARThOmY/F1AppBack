80a7eff4a2add7f38992c7890cd01bc4badd1882
ThomyAshby <ashbythomas0@gmail.com>
2023-05-30 19:34:08 -0300

    Correcciones Team

--- src/main/java/F1api/app/Model/TeamDTO.java
+++ src/main/java/F1api/app/Model/TeamDTO.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeamDTO {
+    private int id;
     private String Name;
     private String Car;
     private String Location;
